476d5ab4b7fceb3c5570f50e4c516e3baec02fa3
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-23 10:33:32 +0200

    klar

--- upp_14.py
+++ upp_14.py
@@ -1,24 +1,28 @@
 FRUITS = ['banana', 'apple', 'orange']
 CARS = ['volvo', 'ford', 'tesla']
-
+STUFF = ['Wallet', 'footrest', 'sofa']
 
 def run():
     basket = [
-        'volvo', 'is', 'an', 'orange', 'apple'
+        'volvo', 'is', 'an', 'orange', 'sofa', 'apple', 'footrest'
     ]
     cars = []
     fruits = []
+    stuff = []
     rest = []
     for item in basket:
         if item in CARS:
             cars.append(item)
         elif item in FRUITS:
             fruits.append(item)
+        elif item in STUFF:
+            stuff.append(item)
         else:
             rest.append(item)
     write_things(cars, 'Cars')
     write_things(fruits, 'Fruits')
     write_things(rest, 'Misc')
+    write_things(stuff, 'Stuff')
 
 
 def write_things(items, kind):
