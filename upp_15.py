987d682ff0421004221c8a7f1ff9b205b916335b
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-24 13:32:25 +0200

    done

---
+++ upp_15.py
@@ -0,0 +1,7 @@
+vowels = ", ".join(['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', ',ö'])
+
+
+string = "Ta in en godtycklig text (testa att copy-paste från lorumipsum.com) och skriv ut hur många vokaler som finns"
+
+
+for ecb in vowels:
